3aa929750f94b18ccce1150ad0b2a1d3b16c00d6
KylerRussell <116309682+KylerRussell@users.noreply.github.com>
2022-11-15 16:39:48 -0600

    Attempt to fix Tensor Flow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowImageDetectionWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowImageDetectionWebcam.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -10,9 +11,35 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.RobotLog;
+import com.qualcomm.robotcore.util.ThreadPool;
+import com.vuforia.Frame;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.function.Consumer;
+import org.firstinspires.ftc.robotcore.external.function.Continuation;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.matrices.MatrixF;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
-
-@TeleOp(name = "TensorFlow Object Detection Webcam", group = "Linear Opmode")
+@TeleOp(name = "TensorFlowObjectDetectionWebcam", group = "Linear Opmode")
 
 public class TensorFlowImageDetectionWebcam extends LinearOpMode {
 
@@ -23,8 +50,9 @@ public class TensorFlowImageDetectionWebcam extends LinearOpMode {
      * has been downloaded to the Robot Controller's SD FLASH memory, it must to be loaded using loadModelFromFile()
      * Here we assume it's an Asset.    Also see method initTfod() below .
      */
-    private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/ExternalLibraries/SleveImageDetection.tflite";
-    // private static final String TFOD_MODEL_FILE  = "/sdcard/FIRST/tflitemodels/CustomTeamModel.tflite";
+    //private static final String TFOD_MODEL_ASSET = "SleveImageDetection.tflite";
+
+    private static final String TFOD_MODEL_FILE  = "/sdcard/FIRST/ExternalLibraries/SleveImageDetection.tflite";
     //This PC\Control Hub v1.0\Internal shared storage\FIRST\ExternalLibraries
 
     private static final String[] LABELS = {
@@ -89,6 +117,7 @@ public class TensorFlowImageDetectionWebcam extends LinearOpMode {
         /** Wait for the game to begin */
         telemetry.addData(">", "Press Play to start op mode");
         telemetry.update();
+        FtcDashboard.getInstance().startCameraStream(vuforia, 0);
         waitForStart();
 
         if (opModeIsActive()) {
@@ -96,6 +125,7 @@ public class TensorFlowImageDetectionWebcam extends LinearOpMode {
                 if (tfod != null) {
                     // getUpdatedRecognitions() will return null if no new information is available since
                     // the last time that call was made.
+
                     List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
                     if (updatedRecognitions != null) {
                         telemetry.addData("# Objects Detected", updatedRecognitions.size());
@@ -108,7 +138,7 @@ public class TensorFlowImageDetectionWebcam extends LinearOpMode {
                             double height = Math.abs(recognition.getTop()  - recognition.getBottom()) ;
                             double width  = Math.abs(recognition.getRight() - recognition.getLeft()) ;
 
-                            telemetry.addData(""," ");
+                            telemetry.addData("","");
 
                             telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100 );
                             telemetry.addData("- Position (Row/Col)","%.0f / %.0f", row, col);
