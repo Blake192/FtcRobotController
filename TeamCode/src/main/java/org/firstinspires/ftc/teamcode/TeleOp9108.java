0ee93aff314f76fe956b553ba6575346c0144f08
Blake192 <73915302+Blake192@users.noreply.github.com>
2022-12-01 17:42:38 -0600

    Make some simple changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp9108.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp9108.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoImpl;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 
@@ -48,6 +50,9 @@ public class TeleOp9108 extends LinearOpMode {
         servoLeft = hardwareMap.get(ServoImplEx.class, "servoLeft");
         servoRight = hardwareMap.get(ServoImplEx.class, "servoRight");
 
+        servoLeft.setPwmEnable();
+        servoRight.setPwmEnable();
+
 
         waitForStart();
 
@@ -73,8 +78,6 @@ public class TeleOp9108 extends LinearOpMode {
             telemetry.addData("x", poseEstimate.getX());
             telemetry.addData("y", poseEstimate.getY());
             telemetry.addData("heading", poseEstimate.getHeading());
-            telemetry.update();
-
             telemetry.addData(" Vaule of: servoLeft.getPwmRange(): ", servoLeft.getPwmRange());
             telemetry.addData("Value of: servoLeft.isPwmEnabled(): ", servoLeft.isPwmEnabled());
             telemetry.addData("Value of servoLeft.getPosition();", servoLeft.getPosition());
@@ -84,38 +87,53 @@ public class TeleOp9108 extends LinearOpMode {
             telemetry.addData("Value of servoRight.getPosition();", servoRight.getPosition());
 
             telemetry.addData("lift.getCurrentPosition(): ", lift.getCurrentPosition());
-
-
             telemetry.update();
+//
+//            telemetry.addData(" Vaule of: servoLeft.getPwmRange(): ", servoLeft.getPwmRange());
+//            telemetry.addData("Value of: servoLeft.isPwmEnabled(): ", servoLeft.isPwmEnabled());
+//            telemetry.addData("Value of servoLeft.getPosition();", servoLeft.getPosition());
+//
+//            telemetry.addData(" Vaule of: servoRight.getPwmRange(): ", servoRight.getPwmRange());
+//            telemetry.addData("Value of: servoRight.isPwmEnabled(): ", servoRight.isPwmEnabled());
+//            telemetry.addData("Value of servoRight.getPosition();", servoRight.getPosition());
+//
+//            telemetry.addData("lift.getCurrentPosition(): ", lift.getCurrentPosition());
+//
+//
+//            telemetry.update();
 
-            if (gamepad1.left_trigger > 0.05 && gamepad1.right_trigger < 0.05) { // Raise up on right trigger
-                lift.setTargetPosition(lift.getCurrentPosition()+100);
+            if (gamepad2.left_trigger > 0.05 && gamepad2.right_trigger < 0.05) { // Raise up on right trigger
+//                lift.setTargetPosition(lift.getCurrentPosition()+100);
+                lift.setPower(1);
                 telemetry.addData("Left Trigger: ", gamepad1.left_trigger);
                 telemetry.update();
             }
 
-            if (gamepad1.right_trigger > 0.05 && gamepad1.left_trigger < 0.05) {
-                lift.setTargetPosition(lift.getCurrentPosition()-100);
+            if (gamepad2.right_trigger > 0.05 && gamepad2.left_trigger < 0.05) {
+//                lift.setTargetPosition(lift.getCurrentPosition()-100);
+                lift.setPower(-1);
             }
 
             else {
-                lift.setTargetPosition(lift.getCurrentPosition());
+//                lift.setTargetPosition(lift.getCurrentPosition());
+                lift.setPower(0);
             }
 
 
 
-
+            float closeDistance = 1.0f;
+            float openDistance = 0.6f;
 
             // Right trigger controller 2
             // Starts closed with cone and clawInvertRight = true
-            if (gamepad2.right_trigger > 0.05 && !clawInvertRight) {
+            if (gamepad1.right_trigger > 0.05 && !clawInvertRight) {
                 // Servo only go 0 to 1
-                servoRight.setPosition(0.5); // Close
+                servoRight.setPosition(closeDistance); // Close
                 clawInvertRight = !clawInvertRight;
             }
 
-            if (gamepad2.right_trigger > 0.05 && clawInvertRight) {
-                servoRight.setPosition(0.25); // Open
+            if (gamepad1.right_trigger > 0.05 && clawInvertRight) {
+                servoRight.setPosition(openDistance); // Open
                 clawInvertRight = !clawInvertRight;
             }
             else {
@@ -124,12 +142,12 @@ public class TeleOp9108 extends LinearOpMode {
 
             // Left trigger controller 2
             // Starts closed with cone and clawInvertLeft = true at start
-            if (gamepad2.left_trigger > 0.05 && !clawInvertLeft) {
-                servoLeft.setPosition(0.5); // Close
+            if (gamepad1.left_trigger > 0.05 && !clawInvertLeft) {
+                servoLeft.setPosition(closeDistance); // Close
                 clawInvertLeft = !clawInvertLeft;
             }
-            if (gamepad2.left_trigger > 0.05 && clawInvertLeft) {
-                servoLeft.setPosition(0.25); // Open
+            if (gamepad1.left_trigger > 0.05 && clawInvertLeft) {
+                servoLeft.setPosition(openDistance); // Open
                 clawInvertLeft = !clawInvertLeft;
             }
             else {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -148,9 +148,9 @@ public class SampleMecanumDrive extends MecanumDrive {
 
         // TODO: reverse any motors using DcMotor.setDirection()
 
-        //leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        //leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
+        //rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // TODO: if desired, use setLocalizer() to change the localization method
